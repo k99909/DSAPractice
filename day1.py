e200a184c964adeb1d33713206d109af3240b067
Kaz DeBear <73315091+k99909@users.noreply.github.com>
2022-06-01 17:39:39 -0700

    JS Intersection

--- day1.py
+++ day1.py
@@ -2,26 +2,28 @@
 #Be sure to return the indices, not the elements themselves.
 #There is guaranteed to be one such pair whose product is the target.
 
-def pair_product(numbers, target_product):
-    previous_nums = {}
-    for index, number in enumerate(numbers):
-       complement = target_product / number
+# def pair_product(numbers, target_product):
+#     previous_nums = {}
+#     for index, number in enumerate(numbers):
+#        complement = target_product / number
 
-       if complement in previous_nums:
-           return (index, previous_nums[complement])
+#        if complement in previous_nums:
+#            return (index, previous_nums[complement])
 
-       previous_nums[number] = index
+#        previous_nums[number] = index
            
 
-#test_00:
-print(pair_product([3, 2, 5, 4, 1], 8)) # -> (1, 3)
-#test_01:
-print(pair_product([3, 2, 5, 4, 1], 10)) # -> (1, 2)
-#test_02:
-print(pair_product([4, 7, 9, 2, 5, 1], 5)) # -> (4, 5)
-#test_03:
-print(pair_product([4, 7, 9, 2, 5, 1], 35)) # -> (1, 4)
-#test_04:
-print(pair_product([3, 2, 5, 4, 1], 10)) # -> (1, 2)
-#test_05:
-print(pair_product([4, 6, 8, 2], 16)) # -> (2, 3)
+# #test_00:
+# print(pair_product([3, 2, 5, 4, 1], 8)) # -> (1, 3)
+# #test_01:
+# print(pair_product([3, 2, 5, 4, 1], 10)) # -> (1, 2)
+# #test_02:
+# print(pair_product([4, 7, 9, 2, 5, 1], 5)) # -> (4, 5)
+# #test_03:
+# print(pair_product([4, 7, 9, 2, 5, 1], 35)) # -> (1, 4)
+# #test_04:
+# print(pair_product([3, 2, 5, 4, 1], 10)) # -> (1, 2)
+# #test_05:
+# print(pair_product([4, 6, 8, 2], 16)) # -> (2, 3)
+
+
